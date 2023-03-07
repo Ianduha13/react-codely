23255a01a50b9b5d62a2cf26bdcdd72ad5289ca1
Ian Duhamel <ianduhamelhayes@gmail.com>
2023-03-07 13:53:10 -0300

    Adding github responses and dashboard iteration

--- src/App.tsx
+++ src/App.tsx
@@ -2,4 +2,4 @@ import { Dashboard } from "./sections/dashboard/Dashboard";
 
 export function App() {
 	return <Dashboard />;
-};
+}
--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,3 +1,23 @@
+import { config } from "../../devdash_config";
+
 export function Dashboard() {
-return<div>Dashboard</div>
+	const title = "DevDash_";
+
+	return (
+		<>
+			<header>
+				<h1>{title}</h1>
+			</header>
+			
+			<section>
+				<ul>
+
+					{config.widgets.map((widget) => (
+
+						<li key={widget.id}>{widget.repository_url}</li>
+					))}
+				</ul>
+			</section>
+		</>
+	);
 }
