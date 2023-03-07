6289b358e85496f2bda79e7aa91527ae34ba12ca
Ian Duhamel <ianduhamelhayes@gmail.com>
2023-03-07 14:53:09 -0300

    Adding dashboard img

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,20 +1,23 @@
-import { config } from "../../devdash_config";
+import { githubApiResponses } from "../../github_api_responses";
+import styles from "./Dashboard.module.scss";
+import { ReactComponent as Lock } from "./lock.svg";
+import { ReactComponent as Unlock } from "./unlock.svg";
 
 export function Dashboard() {
 	const title = "DevDash_";
 
 	return (
 		<>
-			<header>
+			<header className={styles.container}>
 				<h1>{title}</h1>
 			</header>
-			
 			<section>
 				<ul>
-
-					{config.widgets.map((widget) => (
-
-						<li key={widget.id}>{widget.repository_url}</li>
+					{githubApiResponses.map((widget) => (
+						<li key={widget.repositoryData.id}>
+							{widget.repositoryData.archive_url}
+							{widget.repositoryData.private ? <Lock /> : <Unlock />}
+						</li>
 					))}
 				</ul>
 			</section>
